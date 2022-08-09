765e781eca4d9773ce8438df675e4fa997a3903c
Vincent Toon <vincenttoon22@gmail.com>
2022-08-09 12:16:32 -0500

    high score button is there

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
-const questionArr [
+const questionArr = [
     {
 
     }
 ];
+
+// ^^ vv posible variables
+var highscore = document.querySelector(".high-score");
+var clock = document.querySelector(".timer");
+var welcome = document.querySelector(".welcome");
+var rules = document.querySelector(".rules");
+var questions = document.querySelector(".questions");
+var results = document.querySelector(".results");
+var scores = [];
+
+function home() {
+    // create high score button
+    var viewScores = document.createElement("button");
+    viewScores.classList.add("header", "view-score")
+    viewScores.textContent = "High Scores";
+    console.log(viewScores);
+
+    highscore.appendChild(viewScores);
+    
+}
+
+
+// possible functions
+
+home();
+
 
